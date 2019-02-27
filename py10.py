979f409a888c1d3c8f3e15db890e9b8e8651cfdf
Long Nguyen <longnguyen@supermicro.com>
2019-02-26 21:03:03 -0800

    should stop here for the night. Pace myself

---
+++ py10.py
@@ -0,0 +1,25 @@
+# Raptors destroyed the Celtics. Dang man.
+# The young core not meshing with the OGs on the team
+
+print("The backslash '\\' is an escape character")
+print("It's an artifact from back in the days when people had to share a mainframe through an actual physical Terminal")
+
+print("I am 7'8\" tall.") 	#escape double-quote inside string
+print('I am 8\'1" tall.')		#escape single-quote inside string
+
+tabby_cat = "\tI'm tabbed in."		#Notepad++ uses \t for search string too, if you want to replace "    " with tabs
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."		# actually not sure what this does.
+# oh pfft, it just \ the \
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
