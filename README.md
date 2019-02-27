ba6334eb2d88d841b6da339a347df44641d43643
Long Nguyen <longnguyen@supermicro.com>
2019-02-26 21:05:02 -0800

    boop

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # py3dahardway
 Apparently, he updated it to py3.. I didn't even finished learn python the hard way on py2. Here we go again.
 
-This is the definition of insanity. But you need to stick to it. Grind it out.
-It will pay off one day. You have the opportunity that many others do not have.
-Take advantage of the resources given to you. Don't squander your time.
+stick to it. Grind it out.
+
+I need to learn some db stuff.
+
---
+++ py9.py
@@ -0,0 +1,19 @@
+# Ohhh wow there's 52 exercises. So this is about a little more than a 6th of the way there
+# Who's excited for the Nintendo Direct announcement for Pokemon Go tomorrow? I am.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # well, I already know what this does from bash scripting
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+#I know what this does too, it's a block of text
+print("""						# does text appear if i put it behind the #? yes, yes it does.
+	There's something going on here.
+	With the three double-quotes.
+	We'll be able to type as much as we like.
+	Even 4 lines if we want, or 5, or 6.
+	""")
+	# everything inside the triple """ gets printed
+	
+	
