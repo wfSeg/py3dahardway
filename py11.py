7e51c85af5084fb2c6c9e76c2951b898604baad3
Long Nguyen <longnguyen@supermicro.com>
2019-02-26 21:10:18 -0800

    time to go home, live life a little

---
+++ py11.py
@@ -0,0 +1,15 @@
+# I like this part, finally getting somewhere. Inputs!
+
+print("How old are you?", end=' ')
+age = input()
+
+print("How tall are you?", end=' ')
+height = input()
+
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# Oh hey he explains what  - end=' ' - does 
+# This tells print to not end the line with a newline character and go to the next line.
