4740bc56308c7addd44e1c81297ef95c2480ad23
Long Nguyen <longnguyen@supermicro.com>
2019-03-04 20:20:38 -0800

    jk i did something, algorithm saps my will to live

--- py32.py
+++ py32.py
@@ -7,3 +7,33 @@
 
 # It's SO hard getting into it. I get distracted. Sidetracked. Bored.
 # Going to Skim through it.
+# O notation, Omega notation, Theta notation... riiight. 
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# first loop, goes through list
+# also... al gore's rhythms sleepy
+for number in the_count:
+	print(f"This is count {number}")
+	
+for fruit in fruits:
+	print(f"A fruit of type: {fruit}")
+	
+#mixed list
+for i in change:
+	print(f"I got {1}")
+	
+# building dat wal- er, list
+wall = []
+# specify range to count to
+#for i in range(0, 1000):		#let's not do a 1000
+for i in range(0, 6):
+	print(f"Adding Brick #{i} to the Wall")
+	# now adding it to the list
+	wall.append(i)
+
+# showing the wall.. ohboy
+for i in wall:
+	print(f"Brick #{i} was placed.")
