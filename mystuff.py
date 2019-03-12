cee76d5a12c9ce5ddd1ffefa29ad7948289cfb31
Long Nguyen <longnguyen@supermicro.com>
2019-03-11 17:51:15 -0700

    vacation~

---
+++ mystuff.py
@@ -0,0 +1,6 @@
+# a module. simple
+
+def apple():
+	print(" BEHOLD. I AM APPLES ")
+	
+# to use, import mystuff, then mystuff.apple()
---
+++ py40.py
@@ -0,0 +1,11 @@
+# Going to see Captain More Balls tonight, so need to do this now
+
+print("Modules! Classes! Objects! This is \"Object Oriented Programming!\" Woohoo")
+
+import mystuff
+mystuff.apple()
+
+#print(mystuff.tangerine)	# there's no tangerines in mystuff
+# going to have to add it first
+
+# Going on vacation so... will pick this up after that. Hardest part of this is staying consistent for sure.
